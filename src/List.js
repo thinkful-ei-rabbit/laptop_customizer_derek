e49967911e1837ad8f1d65f6f161c4a7c0ffbb38
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-08-25 13:02:01 -0700

    init

--- src/List.js
+++ src/List.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from './Card'
 import './styles/List.css';
 
-export default function List(props) {
-  return (
-    <section className='List'>
-      <header className='List-header'>
-        <h2>{props.header}</h2>
-      </header>
-      <div className='List-cards'>
-        {props.cards.map((card) =>
-          <Card
-            key={card.id}
-            title={card.title}
-            content={card.content}
-          />
-        )}
-        <button
-          type='button'
-          className='List-add-button'
-        >
-          + Add Random Card
-        </button>
-      </div>
-    </section>
-  )
+class List extends Component {
+  static defaultProps = {
+    props: {
+      key: '',
+      header: '',
+      cards: [],
+    }
+  };
+
+  render() {
+    const props = this.props;
+    return (
+      <section className='List'>
+        <header className='List-header'>
+          <h2>{props.header}</h2>
+        </header>
+        <div className='List-cards'>
+          {props.cards.map((card) =>
+            <Card
+              key={card.id}
+              title={card.title}
+              content={card.content}
+            />
+          )}
+          <button
+            type='button'
+            className='List-add-button'
+          >
+            + Add Random Card
+          </button>
+        </div>
+      </section>
+    )
+  }
 }
+
+export default List
