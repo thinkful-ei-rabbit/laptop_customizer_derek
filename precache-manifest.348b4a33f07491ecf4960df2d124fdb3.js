5fcc207d8140060852296c9dbc3295f68ef42959
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-08-28 15:52:34 -0700

    Updates

---
+++ precache-manifest.348b4a33f07491ecf4960df2d124fdb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ddd122649cadc24a6cce5241bc65ac",
+    "url": "/laptop_customizer_derek/index.html"
+  },
+  {
+    "revision": "6145e42195ada575c148",
+    "url": "/laptop_customizer_derek/static/css/main.c68ac268.chunk.css"
+  },
+  {
+    "revision": "ea7060637f343c9028d4",
+    "url": "/laptop_customizer_derek/static/js/2.54fa624f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/laptop_customizer_derek/static/js/2.54fa624f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6145e42195ada575c148",
+    "url": "/laptop_customizer_derek/static/js/main.52eccaf0.chunk.js"
+  },
+  {
+    "revision": "23b906ae72ec3342a352",
+    "url": "/laptop_customizer_derek/static/js/runtime-main.5b7e01f7.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/laptop_customizer_derek/precache-manifest.348b4a33f07491ecf4960df2d124fdb3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/laptop_customizer_derek/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
